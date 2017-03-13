6f127e719fd49035444087ed419c45719a1018f6
Dennis Hedlund <denhed@Denniss-iMac.zyxel.com>
2017-03-13 21:25:27 +0100

    added songlist component

---
+++ client/components/SongList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag'; // för skapa query
+import { graphql } from 'react-apollo'; // för att kunna binda component och query.
+
+class SongList extends Component {
+
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return <li key={song.id} className="collection-item" >{song.title}</li>;
+    });
+
+  }
+
+  render(){
+    //console.log(this.props);
+    // 1. första gången componenten renderas finns inte songs i props = undefined.
+    // 2. vi har en loading property som flaggar att vi hämtar data från servern.
+
+    if (this.props.data.loading) { return <div>Loading... </div> };
+
+    return(
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
+    );
+  }
+}
+
+// definera en query.
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+
+export default graphql(query)(SongList);
