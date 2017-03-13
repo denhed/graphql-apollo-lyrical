b81bf3ff6ef96bde19cc2f70de6444ba053c01b9
Dennis Hedlund <denhed@Denniss-iMac.local>
2017-03-13 20:26:49 +0100

    first working setup of apollo client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://denhed:qwerty@ds011251.mlab.com:11251/lyrical-denhed';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
